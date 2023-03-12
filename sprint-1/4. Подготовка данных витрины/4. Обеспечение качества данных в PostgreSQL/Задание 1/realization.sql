43068e04b23cde5421a3be18282e41b2dc14e0a5
Yulia Bezginova <ybezginova2021@gmail.com>
2023-03-12 20:43:51 +0100

    Constraints, ALTER TABLE

--- sprint-1/4. Подготовка данных витрины/4. Обеспечение качества данных в PostgreSQL/Задание 1/realization.sql
+++ sprint-1/4. Подготовка данных витрины/4. Обеспечение качества данных в PostgreSQL/Задание 1/realization.sql
@@ -1,1 +1,9 @@
--- добавьте код сюда
+ALTER TABLE user_contacts
+ADD CONSTRAINT user_contacts_client_id_fkey 
+FOREIGN KEY (client_id)
+REFERENCES user_attributes (client_id);
+
+-- Добавьте ограничение по внешнему ключу с названием user_contacts_client_id_fkey 
+-- в уже существующую таблицу user_contacts.
+-- Оно должно сделать так, что в поле client_id таблицы user_contacts будут попадать 
+-- только значения client_id из таблицы user_attributes.
