d21dae1582d36aa867da7740468cb79622fde7f6
Yulia Bezginova <ybezginova2021@gmail.com>
2023-03-14 15:38:04 +0100

    Adding a Column

--- sprint-0/3. Витрина наносит ответный удар/5. Как загрузить новый источник данных/Задание 2/realization.sql
+++ sprint-0/3. Витрина наносит ответный удар/5. Как загрузить новый источник данных/Задание 2/realization.sql
@@ -1,5 +1,5 @@
--- DROP TABLE IF EXISTS clients;
+DROP TABLE IF EXISTS "source_systems";
 
-COPY clients (client_firstname, client_lastname, client_email, client_phone, client_city)
+COPY "source_systems" (client_firstname, client_lastname, client_email, client_phone, client_city)
 FROM '/lessons/3. Витрина наносит ответный удар/5. Как загрузить новый источник данных/Задание 2/clients.csv'
 WITH (FORMAT csv, HEADER true);
--- sprint-0/3. Витрина наносит ответный удар/7. Как добавить атрибуты в существующую таблицу/Задание 1/realization.sql
+++ sprint-0/3. Витрина наносит ответный удар/7. Как добавить атрибуты в существующую таблицу/Задание 1/realization.sql
@@ -1,10 +1,8 @@
-DROP TABLE IF EXISTS source_systems;
-
-ALTER TABLE source_systems
+ALTER TABLE "source_systems"
 ADD COLUMN age integer;
 
-UPDATE source_systems
+UPDATE "source_systems"
 SET age = b.age
-FROM "002_BUFF_clients" b
-WHERE clients.client_id = b.client_id;
+FROM "/lessons/3. Витрина наносит ответный удар/7. Как добавить атрибуты в существующую таблицу/Задание 1/002_BUFF_clients.csv" as b
+WHERE "source_systems".client_id = b.client_id;
 
